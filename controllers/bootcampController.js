fb29b22db20a93b44476fc6a8832f0ad7c541fb7
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-09-02 15:27:09 +0430

    Add get-bootcamps by distance route handler

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -1,5 +1,33 @@
-const serviceController = require('./serviceController');
 const Bootcamp = require('../models/bootcampModel');
+const serviceController = require('./serviceController');
+const geocoder = require('../utils/geocoder');
+const catchAsync = require('../utils/catchAsync');
+
+// Get all bootcamps within a certain radius from a point
+// api/v1/bootcamps/within/:distance/zipcode/:zipcode/unit/:unit
+exports.getBootcampsWithinRadius = catchAsync(async (req, res, next) => {
+  const { distance, zipcode, unit } = req.params;
+  // Get lat/lng from geocoder
+  const location = (await geocoder.geocode(zipcode))[0];
+  const lat = location.latitude;
+  const lng = location.longitude;
+
+  // Calc radius using radians
+  // Divide by the radius of the earth
+  const radius = unit === 'km' ? distance / 6378.1 : distance / 3963.2;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: bootcamps.length,
+    data: {
+      bootcamps
+    }
+  });
+});
 
 // CRUD operations Handlers ( Create, Read, Update, Delete)
 exports.getAllBootcamps = serviceController.getAll(Bootcamp);
--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -5,6 +5,10 @@ const bootcampController = require('../controllers/bootcampController');
 const router = express.Router();
 
 router
+  .route('/within/:distance/zipcode/:zipcode/unit/:unit')
+  .get(bootcampController.getBootcampsWithinRadius);
+
+router
   .route('/')
   .get(bootcampController.getAllBootcamps)
   .post(bootcampController.createNewBootcamp);
